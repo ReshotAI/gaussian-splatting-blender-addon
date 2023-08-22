e21b2f2a943149500d1a7bbe393a6acb6bd7d59f
Alexandre Carlier <alexandre.carlier01@gmail.com>
2023-08-22 15:15:27 +0200

    Update

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ bl_info = {
 import bpy
 
 
+class OBJECT_OT_AddTenCircles(bpy.types.Operator):
+    bl_idname = "object.add_ten_circles"
+    bl_label = "Add 10 Circles"
+    bl_description = "Adds 10 circle meshes to the scene"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        for i in range(10):
+            bpy.ops.mesh.primitive_circle_add()
+        return {'FINISHED'}
+
+
 class GaussianSplattingPanel(bpy.types.Panel):
     
     bl_space_type = "VIEW_3D"
@@ -21,14 +33,16 @@ class GaussianSplattingPanel(bpy.types.Panel):
     def draw(self, context):
         row = self.layout.row()
 
-        row.operator("mesh.primitive_cube_add", text="Add Cube", icon="CUBE")
+        row.operator(OBJECT_OT_AddTenCircles.bl_idname, text="Add 10 Circles")
 
 
 def register():
     bpy.utils.register_class(GaussianSplattingPanel)
+    bpy.utils.register_class(OBJECT_OT_AddTenCircles)
 
 def unregister():
     bpy.utils.unregister_class(GaussianSplattingPanel)
+    bpy.utils.unregister_class(OBJECT_OT_AddTenCircles)
 
 if __name__ == "__main__":
     register()
